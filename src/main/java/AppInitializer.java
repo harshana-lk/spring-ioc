4ae0fb5ac8af383728ac54e5fe391433ebe2fb59
Harshana Jayaweera <mailtoharshanaonlive@gmail.com>
2023-11-19 22:57:56 +0530

    Initial commit

---
+++ src/main/java/AppInitializer.java
@@ -0,0 +1,6 @@
+public class AppInitializer {
+    public static void main(String[] args) {
+        Course fullstackCourse = new FullstackCourse();
+        System.out.println(fullstackCourse.getProgrammeName());
+    }
+}
---
+++ src/main/java/FullstackCourse.java
@@ -0,0 +1,6 @@
+public class FullstackCourse implements Course {
+    @Override
+    public String getProgrammeName(){
+        return "Full-Stack";
+    }
+}
